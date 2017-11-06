3f21586cb5c1f88bf491e90404d5d3d37c2ee7fc
Guillaume EHRET <guillaume@dev-mind.fr>
2017-11-06 23:52:57 +0100

    Updates project name

--- src/main/java/com/devmind/performance/JugLausanneApplication.java
+++ src/main/java/com/devmind/performance/JugLausanneApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Devoxx2017Application {
+public class JugLausanneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Devoxx2017Application.class, args);
+		SpringApplication.run(JugLausanneApplication.class, args);
 	}
 }
--- src/test/java/com/devmind/performance/JugLausanneApplicationTests.java
+++ src/test/java/com/devmind/performance/JugLausanneApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Devoxx2017ApplicationTests {
+public class JugLausanneApplicationTests {
 
 	@Test
 	public void contextLoads() {
